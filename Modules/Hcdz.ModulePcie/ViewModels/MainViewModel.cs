cbc6b93fea678f8763e10d632d8d06d99e880a3e
zhw <zhanghw@pvirtech.com>
2017-09-12 15:55:27 +0800

    thread

--- Modules/Hcdz.ModulePcie/ViewModels/MainViewModel.cs
+++ Modules/Hcdz.ModulePcie/ViewModels/MainViewModel.cs
@@ -4,11 +4,13 @@ using Prism.Events;
 using Prism.Mvvm;
 using Prism.Regions;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
-
+using System.IO;
 namespace Hcdz.ModulePcie.ViewModels
 {
     public class MainViewModel: BindableBase
@@ -16,13 +18,76 @@ namespace Hcdz.ModulePcie.ViewModels
 		private readonly IEventAggregator _eventAggregator;
 		private readonly IUnityContainer _container;
 		private readonly IRegionManager _regionManager;
-		private readonly IServiceLocator _serviceLocator; 
+		private readonly IServiceLocator _serviceLocator;
+		private ConcurrentQueue<int> queue;
+		private bool IsCompleted=false;
 		public MainViewModel(IUnityContainer container, IEventAggregator eventAggregator, IRegionManager regionManager, IServiceLocator serviceLocator)
 		{
 			_container = container;
 			_eventAggregator = eventAggregator;
 			_regionManager = regionManager; 
-			_serviceLocator = serviceLocator; 
+			_serviceLocator = serviceLocator;
+			 queue = new ConcurrentQueue<int>();
+			 Initializer();
+		}
+
+		private void Initializer()
+		{
+			Thread readThread = new Thread(new ThreadStart(ReadDMA));
+			readThread.IsBackground = true;
+			readThread.Start(); 
+			Thread writeThread = new Thread(new ThreadStart(WriteDMA));
+			writeThread.IsBackground = true;
+			writeThread.Start();
+		}
+		private List<int> s1 = new List<int>();
+		private List<int> s2 = new List<int>();
+		private void WriteDMA()
+		{
+			for (int i = 0; i < 10000; i++)
+			{
+				queue.Enqueue(i);
+			}
+			
+		}
+
+		private void ReadDMA()
+		{
+			while (!IsCompleted)
+			{
+				if (queue.IsEmpty)
+				{
+					IsCompleted = true;
+				}
+				else
+				{
+					int result;
+					if (queue.TryDequeue(out result))
+					{
+						if (result%2==0)
+						{
+							ThreadPool.QueueUserWorkItem(WriteBar); 
+						}
+						else
+						{
+							Task.Run(() => {
+								using (var sw = new StreamWriter("D:\\ss1.txt",true))
+								{
+									sw.WriteLine(result);
+								}
+							});
+						}
+					}
+				}
+			}
+		}
+
+		private void WriteBar(object state)
+		{
+			using (StreamWriter sw = new StreamWriter("D:\\ss.txt", true))
+			{
+				sw.WriteLine(1);
+			}
 		}
 	}
 }
